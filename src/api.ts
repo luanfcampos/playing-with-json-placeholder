2e28683c853fafb643b9452932ad37b1dec45f00
Luan Ferreira Campos <lfcampos.eab@gmail.com>
2022-06-27 11:28:38 -0300

    update

--- src/api.ts
+++ src/api.ts
@@ -1,20 +1,16 @@
+import axios from "axios"
 const BASE = 'https://jsonplaceholder.typicode.com'
 
 export const api = {
     getAll: async () => {
-        let response = await fetch(`${BASE}/posts`)
-        let json = await response.json()
-        return json
+        let  response = await axios.get(`${BASE}/posts`)
+        return response.data
     },
     addNew: async (title: string, body: string, userId: number) => {
-        let response = await fetch(`${BASE}/posts`,{
-        method: 'POST',
-        body: JSON.stringify({title, body, userId}),
-        headers: {
-            'Content-Type': 'application/json'
-        }
+        let response = await axios.post(`${BASE}/posts`, {
+            title, body, userId
         })
-        let json = await response.json()
-        return json
+
+        return response.data
     }
 }
